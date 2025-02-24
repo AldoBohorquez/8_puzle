cccaebe69c907b5f78769d06b21c4356688638f7
Aldo Omar Bohorquez Bastidas <aldomarscr@gmail.com>
2025-02-23 20:41:12 -0600

    creacion de variables y funciones tablero
    se crearon variables para la posición
    se crearon los arreglos bidimensionales para el tablero inicial y objetivo
    
    se crearon arreglos tridimensionales para almacenar los estados
    
    se creo la funcion generar tablero aleatorio
    
    se creo la funcion verificar si tiene solucion

--- src/app/components/puzzle/puzzle.component.ts
+++ src/app/components/puzzle/puzzle.component.ts
@@ -1,6 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 
+type Posicion = {
+  fila: number;
+  columna: number;
+};
+
 @Component({
   selector: 'app-puzzle',
   imports: [CommonModule],
@@ -10,5 +15,50 @@ import { Component, OnInit } from '@angular/core';
 export class PuzzleComponent implements OnInit {
   ngOnInit(): void {
     // Se genera el puzzle al presionar el botón
+  }
+
+  tamaño = 3;
+  tableroInicial: number[][] = [];
+  tableroObjetivo: number[][] = [];
+  pasosSolucion: number[][][] = [];
+  estadosExplorados: number[][][] = [];
+
+  generarTableroAleatorio(): number[][] {
+    //crea un arreglo de tamaño x tamaño o 3*3 dando 9 y siendo hasta 8 numeros
+    const numeros = Array.from(
+      { length: this.tamaño * this.tamaño },
+      (_, i) => i
+    );
+    //desordena los numeros de arreglo
+    numeros.sort(() => Math.random() - 0.5);
+
+    //crea un arreglo de tablero de 3*3
+    const tablero: number[][] = [];
+
+    //recorrido para llenar la variable tablero
+    for (let i = 0; i < this.tamaño; i++) {
+      //se hace un push de los numeros desordenados en el tablero de 3*3
+      tablero.push(numeros.slice(i * this.tamaño, (i + 1) * this.tamaño));
+    }
+    return tablero;
+  }
+
+  tieneSolucion(tablero: number[][]): boolean {
+    //se crea un arreglo de numeros en linea y se quita el 0 que es el espacio vacio para ver las inversiones
+    const numerosLinea = tablero.flat().filter((n) => n !== 0);
+
+    //se crea una variable de inversiones donde si es par tiene solucion y si es impar no tiene solucion
+    let inversiones = 0;
+
+    //recorrido para ver las inversiones
+    for (let i = 0; i < numerosLinea.length; i++) {
+      for (let j = i + 1; j < numerosLinea.length; j++) {
+        if (numerosLinea[i] > numerosLinea[j]) {
+          inversiones++;
+        }
+      }
+    }
+    //retorna si es par o impar
+    return inversiones % 2 === 0;
   }
 }
